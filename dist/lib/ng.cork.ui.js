71f8a07d0a7f673d1037508c1c4351f4a0039764
Andre Torgal <andre.torgal@gmail.com>
2015-04-09 01:25:51 +0100

    chore: initial commit

---
+++ dist/lib/ng.cork.ui.js
@@ -0,0 +1,10 @@
+(function (angular) {
+    'use strict';
+
+    var module = angular.module('ng.cork.ui.enter-click', [
+        'ng.cork.ui.enter-click',
+        'ng.cork.ui.enter-click.focus-on',
+        'ng.cork.ui.enter-click.stop-propagation'
+    ]);
+
+})(angular);
---
+++ src/lib/ng.cork.ui.enter-click/enterClick.spec.js
@@ -0,0 +1,14 @@
+describe('ng.cork.ui.enter-click', function () {
+    'use strict';
+
+    beforeEach(module('ng.cork.ui.enter-click'));
+
+    describe('corkUiEnterClick', function () {
+
+        it('should pass a dummy test.', inject(function () {
+            expect(true).toBe(true);
+        }));
+
+    });
+
+});
